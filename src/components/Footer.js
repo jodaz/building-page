35d3908b6ea086cdd1634523b731e10e81dae0fe
Jesus Ordosgoitty <jesuodz@gmail.com>
2022-09-06 20:53:29 -0400

    Fix vanguarddevs link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import Box from '@mui/material/Box'
-import { Link } from 'react-router-dom'
+import Link from '@mui/material/Link'
 
 const Footer = () => (
     <Box sx={{
@@ -14,9 +14,27 @@ const Footer = () => (
         <Box sx={{
             display: 'flex',
             alignItems: 'center',
-            alignSelf: 'center'
+            alignSelf: 'center',
+            width: '13rem',
+            justifyContent: 'space-between'
         }}>
-            © 2022 <Link target="_blank" to={{ pathname: "vanguarddevs.com" }}>VanguardDevs</Link>
+            © 2022{'   '}
+            <Link
+                href="https://vanguarddevs.com"
+                rel="external"
+                target="_blank"
+                sx={{
+                    transition: '0.3s',
+                    padding: '0.25rem 0.5rem',
+                    fontWeight: '500',
+                    textDecoration: 'none',
+                    fontSize: '1.05rem',
+                    backgroundColor: 'rgb(26, 26, 197)',
+                    color: '#fff'
+                }}
+            >
+                VanguardDevs
+            </Link>
         </Box>
     </Box>
 );
